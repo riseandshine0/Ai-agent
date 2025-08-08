1edadbaa30cc92ee20f9c9ab5c3bf6a1c544b800
riseandshine0 <riseandshine@posteo.de>
2025-08-08 23:47:23 +0200

    CH1 L6

--- main.py
+++ main.py
@@ -25,8 +25,11 @@ def main():
     response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if "--verbose" in sys.argv[2:]:
+        print(f"User prompt: {user_promt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
